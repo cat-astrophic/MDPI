0bb099a7c6b10bd5e74f8bb1b3fdeda682fdee56
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2019-10-18 11:01:29 -0400

    creates a list of urls for each published paper

--- MDPI.py
+++ MDPI.py
@@ -23,26 +23,32 @@ for i in range(19411):
     
     # Getting the raw data
     
-    page = urllib.request.Request(url, headers = {'User-Agent': 'Mozilla/5.0'})
-    response = urllib.request.urlopen(page)
-    soup = bs(response, 'html.parser')
-    
-    # Making a list of the paper links from that results page
-    
-    data = soup.find_all('a')
-    articles = []
-    
-    for dat in data:
+    try:
         
-        if str(dat)[0:21] == '<a class="title-link"':
+        page = urllib.request.Request(url, headers = {'User-Agent': 'Mozilla/5.0'})
+        response = urllib.request.urlopen(page)
+        soup = bs(response, 'html.parser')
+        
+        # Making a list of the paper links from that results page
+        
+        data = soup.find_all('a')
+        articles = []
+        
+        for dat in data:
             
-            articles.append(dat)
-    
-    for art in articles:
+            if str(dat)[0:21] == '<a class="title-link"':
+                
+                articles.append(dat)
         
-        c1 = str(art).find('href="')
-        c2 = str(art)[c1+6:].find('"')
-        links.append('https://www.mdpi.com' + str(art)[c1+6:c1+6+c2])
+        for art in articles:
+            
+            c1 = str(art).find('href="')
+            c2 = str(art)[c1+6:].find('"')
+            links.append('https://www.mdpi.com' + str(art)[c1+6:c1+6+c2])
+        
+    except:
+        
+        continue
         
 with open('C:/Users/User/Documents/Data/MDPI/links.txt', 'a') as file:
     
